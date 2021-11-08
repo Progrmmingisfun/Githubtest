d1a331333cd8d3bf98ab8ac6aaedcb926691727b
Progrmmingisfun <simi4real@live.co.uk>
2021-11-08 16:19:56 +0100

    started working on file converter

---
+++ File_Converter.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 8080
+var path = require('path');
+const fs = require("fs");
+
+
+
+
+
+
+app.get('/convert/topdf', function(request, response) {
+	response.sendFile(path.join(__dirname + '/File_converter.html'));
+});
+
+
+app.listen(port,() =>{
+console.log('App is listening on Port ${port}');
